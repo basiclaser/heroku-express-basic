f441b2ef1e69967fd1eb258dc819747df0f8dc94
z-memory <dont@forgetmy.email>
2022-02-17 19:12:24 +0100

    add ejs template for homepage

--- index.js
+++ index.js
@@ -2,30 +2,20 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.set("view engine", "ejs");
+
 app.use((req, res, next) => {
   // middleware
   console.log(req.method, req.path);
   next();
 }); // console.log the request before doing anything else
 
-app.use(express.static("public")); // serve static files from the /public folder
-
-app.route("/download").get((req, res) => {
-  console.log(
-    "date: " + new Date().toDateString() + " route: /download method: get "
-  );
-  const file = `${__dirname}/public/main.css`;
-  console.log(__dirname);
-  res.download(file);
-});
+// app.use(express.static("public")); // serve static files from the /public folder
 
 app
   .route("/")
   .get((req, res) => {
-    console.log(
-      "date: " + new Date().toDateString() + " route: / method: get "
-    );
-    res.sendFile(`${__dirname}/public/qwdqwdqwdqwd.html`);
+    res.render("index.ejs", { user: "bob" });
   })
   .post((req, res) => {
     console.log(
